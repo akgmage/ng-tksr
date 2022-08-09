865b0f332618e8c924acd030061ea30b02ca7e4a
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-10 00:42:42 +0530

    show images and buton below image in same line

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,20 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'Batman & Robbin';
-  bestMovieEver = false;
-  favFlop = {
-    title: 'wing commander',
-    yearReleased: 1999,
-    rating: 'PG-13',
-    imdbLink: 'https://www.google.com',
-    soundTrack: {
-      youtubeLink: 'https://www.youtube.com',
-    },
-  };
-  dataClicked(event: Event) {
-    const target = event.target as HTMLElement;
-    console.log('year clicked', target.innerText);
-  }
-}
+export class AppComponent {}
