836eaa032696168b00c73b41714b5c166b3a7754
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-09 20:04:28 +0530

    add two way data binding

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './product/product.component';
-
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    ProductComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ProductComponent],
+  imports: [CommonModule, FormsModule],
 })
-export class UserModule { }
+export class UserModule {}
