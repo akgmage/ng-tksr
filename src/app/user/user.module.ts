1b8f75eeaeb0e27323a1962ea6c5bedc4de959f4
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-10 15:57:27 +0530

    add product detail component inside user module

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './product/product.component';
 import { FormsModule } from '@angular/forms';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
-  declarations: [ProductComponent],
+  declarations: [ProductComponent, ProductDetailComponent],
   imports: [CommonModule, FormsModule],
 })
 export class UserModule {}
